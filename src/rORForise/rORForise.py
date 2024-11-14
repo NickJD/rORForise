6d21bd4f4227ae9f8a574f145fa0ad902dac6e6e
Nick Dimonaco <dimonacn@mcmaster.ca>
2024-11-14 08:44:06 -0500

    v0.0.2: First working beta release of rORForise.

--- src/rORForise/rORForise.py
+++ src/rORForise/rORForise.py
@@ -1,41 +1,52 @@
+import argparse
 import evaluate
 
-directory = "../../Genome_Processing"
 
-genomes = ["Mycoplasma_genitalium_G37"] 
-#genomes = ["Staphylococcus_aureus_502A"]
-#genomes = ["Escherichia_coli_k_12"]
+def parse_args():
+    parser = argparse.ArgumentParser(description="Process genome evaluation parameters.")
 
-GC_prob = 0.3169 # Myco
-#GC_prob = 0.3292 # Staph
-#GC_prob = 0.5080 # Ecoli
+    parser.add_argument('-d', '--directory', type=str, required=True,
+                        help="Path to the main directory containing genome processing subdirectories.")
 
-fragmentation_types = ["ART_errFree"]
-subgroups = ['Combined']
-#subgroups = ['Combined_FGS_sanger_5']
+    parser.add_argument('-g', '--genomes', type=str, nargs='+', required=True,
+                        help="List of genome names to process. Example: 'Mycoplasma_genitalium_G37 Staphylococcus_aureus_502A'")
 
-methods = ["FragGeneScan"] #,"Pyrodigal","Naive-StORF-V1"]#,"Naive-StORF-V2","Naive-StORF-V3"] 
-#methods = ["Pyrodigal"]
-#methods = ["Naive-StORF-V1"]
+    parser.add_argument('--gc_prob', type=float, required=True,
+                        help="GC probability of the genome being processed (e.g., 0.3169 for Mycoplasma genitalium).")
+
+    parser.add_argument('-f', '--fragmentation_types', type=str, nargs='+', required=True,
+                        help="List of fragmentation types to use (e.g., 'ART_errFree').")
+
+    parser.add_argument('-s', '--subgroups', type=str, nargs='+', required=True,
+                        help="List of subgroups for evaluation (e.g., 'Combined').")
+
+    parser.add_argument('-m', '--methods', type=str, nargs='+', required=True,
+                        help="List of methods for evaluation (e.g., 'FragGeneScan').")
+
+    return parser.parse_args()
+
 
 def main():
-    for genome_name in genomes:
-        print(genome_name)
-        datadir = directory + "/" + genome_name + "/Processing"
-        intersect_bed_filename = datadir + "/"+genome_name+"_Reads_Intersect_CDS.tsv.gz"
+    args = parse_args()
 
-        for method in methods:
+    for genome_name in args.genomes:
+        print(genome_name)
+        datadir = f"{args.directory}/{genome_name}/Processing"
+        intersect_bed_filename = f"{datadir}/{genome_name}_Reads_Intersect_CDS.tsv.gz"
+
+        for method in args.methods:
             print(method)
 
-            for fragmentation_type in fragmentation_types:
+            for fragmentation_type in args.fragmentation_types:
                 print(fragmentation_type)
 
-                for group in subgroups:
-                    (total_preds, preds) = evaluate.read_preds(directory, genome_name, method, fragmentation_type, group)
-                    print("Number of predictions made for reads", total_preds, sep = "\t")
-                    evaluate.evaluate(genome_name, GC_prob, preds, intersect_bed_filename, method)
+                for group in args.subgroups:
+                    total_preds, preds = evaluate.read_preds(args.directory, genome_name, method, fragmentation_type,
+                                                             group)
+                    print("Number of predictions made for reads", total_preds, sep="\t")
+                    evaluate.evaluate(genome_name, args.gc_prob, preds, intersect_bed_filename, method)
 
 
-                    
-main()
-# use as follows: python rORForise.py
+if __name__ == "__main__":
+    print("Running rORForise")
+    main()
---
+++ src/rORForise/rORForise_OLD.py
@@ -0,0 +1,41 @@
+import evaluate
+
+directory = "../../Genome_Processing"
+
+genomes = ["Mycoplasma_genitalium_G37"] 
+#genomes = ["Staphylococcus_aureus_502A"]
+#genomes = ["Escherichia_coli_k_12"]
+
+GC_prob = 0.3169 # Myco
+#GC_prob = 0.3292 # Staph
+#GC_prob = 0.5080 # Ecoli
+
+fragmentation_types = ["ART_errFree"]
+subgroups = ['Combined']
+#subgroups = ['Combined_FGS_sanger_5']
+
+methods = ["FragGeneScan"] #,"Pyrodigal","Naive-StORF-V1"]#,"Naive-StORF-V2","Naive-StORF-V3"] 
+#methods = ["Pyrodigal"]
+#methods = ["Naive-StORF-V1"]
+
+def main():
+    for genome_name in genomes:
+        print(genome_name)
+        datadir = directory + "/" + genome_name + "/Processing"
+        intersect_bed_filename = datadir + "/"+genome_name+"_Reads_Intersect_CDS.tsv.gz"
+
+        for method in methods:
+            print(method)
+
+            for fragmentation_type in fragmentation_types:
+                print(fragmentation_type)
+
+                for group in subgroups:
+                    (total_preds, preds) = evaluate.read_preds(directory, genome_name, method, fragmentation_type, group)
+                    print("Number of predictions made for reads", total_preds, sep = "\t")
+                    evaluate.evaluate(genome_name, GC_prob, preds, intersect_bed_filename, method)
+
+
+                    
+main()
+# use as follows: python rORForise.py
